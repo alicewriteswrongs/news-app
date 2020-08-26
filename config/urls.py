3e4d360f7b44b46528e4a4d070afefaaf54e4ea7
Alice Pote <alice.writes.wrongs@gmail.com>
2020-08-26 10:14:59 -0400

    removing some user stuff I won't need

--- config/urls.py
+++ config/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    path("users/", include("news_app.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
