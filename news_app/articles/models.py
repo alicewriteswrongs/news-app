98d9ad701202a776c1aa2be6ce7b5ac17c18c456
Alice Pote <alice.writes.wrongs@gmail.com>
2020-08-27 09:43:37 -0400

    ensure no duplicate articles, and fix issue with HTML description

--- news_app/articles/models.py
+++ news_app/articles/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from dateutil import parser
 from datetime import datetime
 import feedparser
+from bs4 import BeautifulSoup
 
 TOP_HEADLINES = "top-headlines"
 EVERYTHING = "everything"
@@ -70,21 +71,21 @@ class NewsAPIQuery(models.Model):
             self.base_api_url(),
             params = self.params(),
             headers = { "X-Api-Key": settings.NEWS_API_KEY }
+                "url" = article["url"],
         )
         return response.json()
 
     def fetch_and_save_new_articles(self):
         for article in self.get_results()["articles"]:
-            # this needs to be a get_or_create or something
-            # to avoid dupes
-            new_article = Article(
-                source = article["source"]["name"],
-                title = article["title"],
-                description = article["description"],
+            Article.objects.get_or_create(
                 url = article["url"],
-                publish_date = parser.isoparse(article["publishedAt"])
+                defaults = {
+                    "source" : article["source"]["name"],
+                    "title" : article["title"],
+                    "description" : article["description"] or "",
+                    "publish_date" : parser.isoparse(article["publishedAt"])
+                }
             )
-            new_article.save()
 
     def __str__(self):
         repr = "{}".format(QUERY_TYPE_DICT[self.query_type])
@@ -110,18 +111,20 @@ class RSSFeed(models.Model):
     url = models.URLField(
         max_length=300
     )
-    
+
     def fetch_and_save_new_articles(self):
         feed = feedparser.parse(self.url)
         for entry in feed.entries:
-            new_article = Article(
-                source = self.display_name,
-                title = entry['title'],
-                description=  entry['description'],
+            Article.objects.update_or_create(
                 url = entry['link'],
-                publish_date = datetime(*entry['published_parsed'][:6])
+                defaults= {
+                    "source" : self.display_name,
+                    "title" : entry['title'],
+                    "description":  BeautifulSoup(entry['description']).text,
+                    "url" : entry['link'],
+                    "publish_date" : datetime(*entry['published_parsed'][:6])
+                }
             )
-            new_article.save()
 
     def __str__(self):
         return self.display_name
