ff03b41f2fdd72b15ae32d1b18f4c69aac99fdc6
Alice Pote <alice.writes.wrongs@gmail.com>
2020-08-27 08:59:16 -0400

    add to admin

--- news_app/articles/admin.py
+++ news_app/articles/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from news_app.articles.models import NewsAPIQuery, Article
+from news_app.articles.models import NewsAPIQuery, Article, RSSFeed
 
 # Register your models here.
 
 admin.site.register(NewsAPIQuery)
 admin.site.register(Article)
+admin.site.register(RSSFeed)
