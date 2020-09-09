48f00695d349921440f2ca6b13983a8ee58c0b41
Alice Pote <alice.writes.wrongs@gmail.com>
2020-09-09 10:53:25 -0400

    or that

--- config/settings/production.py
+++ config/settings/production.py
@@ -64,7 +64,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'news_app/static'),
+    os.path.join(BASE_DIR, '../../news_app/static')
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
