724041e617b74803f77c4608cd85d2c57c01e637
Alice Pote <alice.writes.wrongs@gmail.com>
2020-09-09 09:52:20 -0400

    trying to configure whitenoise

--- config/settings/base.py
+++ config/settings/base.py
@@ -131,6 +131,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 # STATIC
